ac2dd690845184a2d88f037dae5b3952f9cfa9cf
Sumuditha-Janith <sumuditha356@gmail.com>
2025-06-14 01:18:39 +0530

    login servlet

---
+++ src/main/java/edu/ijse/gdse71/cms/controller/LoginServlet.java
@@ -0,0 +1,39 @@
+package edu.ijse.gdse71.cms.controller;
+
+import edu.ijse.gdse71.cms.dao.UserDAO;
+import edu.ijse.gdse71.cms.model.User;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private UserDAO userDAO;
+    public void init() { userDAO = new UserDAO(); }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user = userDAO.validateUser(username, password);
+
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            response.sendRedirect("dashboard");
+        } else {
+            request.setAttribute("errorMessage", "Invalid username or password. Please try again.");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,14 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 6/14/2025
+  Time: 1:12 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
+
